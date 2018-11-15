35300ed76193aa2631e5a96fe60a0f0d2e17c0d1
Xander Guzman <xander.guzman@xanderguzman.com>
2018-11-15 07:28:27 -0700

    - Restored the better-defaults path to the 02-globals.el file. - Removed the redundant installation of org-mode

---
+++ .emacs.d/customizations/02-globals.el
@@ -0,0 +1,7 @@
+;;; better-defaults --- Summary
+;;; Commentary:
+;;; Code:
+(require 'better-defaults)
+(add-to-list 'load-path (file-name-directory (locate-library "better-defaults")))
+(provide '02-globals)
+;;; 02-globals.el ends here
