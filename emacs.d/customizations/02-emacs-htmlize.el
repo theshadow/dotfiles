f9ca77e2e5fc930a81b0e94177783383e7648cf5
Xander Guzman <xander.guzman@xanderguzman.com>
2018-12-18 10:22:34 -0700

    - Updated the makefile to clone the htmlize repo - Updated the `02-org-mode.el` so that it defines a list of languages allowed in SRC blocks for evaluation. - Updated the gitignore to include JetBrain's `.Idea/` directory

---
+++ emacs.d/customizations/02-emacs-htmlize.el
@@ -0,0 +1,6 @@
+;;; 02-emacs-htmlize.el --- Summary
+;;; Commentary:
+;;; Code:
+(add-to-list 'load-path (concat user-emacs-directory "customizations/emacs-htmlize/"))
+(provide '02-emacs-htmlize)
+;;; 02-emacs-htmlize.el ends here
--- emacs.d/customizations/02-org-mode.el
+++ emacs.d/customizations/02-org-mode.el
@@ -1,5 +1,20 @@
 ;;; 02-org-mode.el --- Summary
 ;;; Commentary:
 ;;; Code:
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)
+   (emacs-lisp . t)
+   (shell . t)
+   (js . t)
+   (gnuplot . t)
+   (plantuml . t)
+   (sql . t)
+   (ditaa . t)
+   (haskell . t)
+   (C . t)
+   (latex . t)
+   (R . t)
+   ))
 (provide '02-org-mode)
 ;;; 02-org-mode.el ends here
