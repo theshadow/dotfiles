720d725c4cc837d45c495a4d5c47f6ce48b3913f
Xander Guzman <xander.guzman@xanderguzman.com>
2018-12-31 16:19:42 -0700

    - stuff

--- emacs.d/customizations/02-org-mode.el
+++ emacs.d/customizations/02-org-mode.el
@@ -19,5 +19,23 @@
 
 (add-to-list 'load-path (concat user-emacs-directory "customizations/org-reveal/"))
 
+(use-package org
+  :config
+  (progn
+    (defun imalison:org-inline-css-hook (exporter)
+      "Insert custom inline css to automatically set the
+background of code to whatever theme I'm using's background"
+      (when (eq exporter 'html)
+        (let* ((my-pre-bg (face-background 'default))
+               (my-pre-fg (face-foreground 'default)))
+          (setq
+           org-html-head-extra
+           (concat
+            org-html-head-extra
+            (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
+                    my-pre-bg my-pre-fg))))))
+
+    (add-hook 'org-export-before-processing-hook 'imalison:org-inline-css-hook)))
+
 (provide '02-org-mode)
 ;;; 02-org-mode.el ends here
